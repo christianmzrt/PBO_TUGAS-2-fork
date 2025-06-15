d4b0cb91ad2016651974e218a95d467cea2dce06
bintang <kadekbintang443@gmail.com>
2025-06-15 17:19:19 +0800

    menambahkan get dalam Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -6,10 +6,58 @@ public class Customer {
     private String email;
     private String phone;
 
+    // Constructor kosong (untuk deserialisasi JSON)
+    public Customer() {}
+
+    // Constructor lengkap
     public Customer(int id, String name, String email, String phone) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+    }
+
+    // Getter dan Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // Optional: toString()
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
