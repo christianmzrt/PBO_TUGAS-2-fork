7ce695d798db8e24f690980e411aefdd677bebdc
bintang <kadekbintang443@gmail.com>
2025-06-15 17:20:11 +0800

    menambahkan get dalam Review.java

--- src/model/Review.java
+++ src/model/Review.java
@@ -6,10 +6,59 @@ public class Review {
     private String title;
     private String content;
 
+    // Constructor kosong (untuk deserialisasi JSON)
+    public Review() {}
+
+    // Constructor lengkap
     public Review(int booking, int star, String title, String content) {
         this.booking = booking;
         this.star = star;
         this.title = title;
         this.content = content;
     }
+
+    // Getter dan Setter
+    public int getBooking() {
+        return booking;
+    }
+
+    public void setBooking(int booking) {
+        this.booking = booking;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // Optional: toString()
+    @Override
+    public String toString() {
+        return "Review{" +
+                "booking=" + booking +
+                ", star=" + star +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
+
