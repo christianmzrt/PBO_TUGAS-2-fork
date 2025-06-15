5060a174a67d4ef732064a40b564b5fed3057381
bintang <kadekbintang443@gmail.com>
2025-06-15 17:18:16 +0800

    menambahkan get dalam Booking.java

--- src/model/Booking.java
+++ src/model/Booking.java
@@ -15,17 +15,131 @@ public class Booking {
     private boolean hasCheckedin;
     private boolean hasCheckedout;
 
-    public Booking(boolean hasCheckedout, boolean hasCheckedin, String paymentStatus, int finalPrice, Integer voucher, int price, LocalDateTime checkoutDate, LocalDateTime checkinDate, int roomType, int customer, int id) {
-        this.hasCheckedout = hasCheckedout;
-        this.hasCheckedin = hasCheckedin;
-        this.paymentStatus = paymentStatus;
-        this.finalPrice = finalPrice;
-        this.voucher = voucher;
-        this.price = price;
-        this.checkoutDate = checkoutDate;
-        this.checkinDate = checkinDate;
-        this.roomType = roomType;
-        this.customer = customer;
+    // Constructor kosong (penting untuk JSON deserialization)
+    public Booking() {
+    }
+
+    // Constructor lengkap
+    public Booking(int id, int customer, int roomType, LocalDateTime checkinDate, LocalDateTime checkoutDate,
+                   int price, Integer voucher, int finalPrice, String paymentStatus,
+                   boolean hasCheckedin, boolean hasCheckedout) {
         this.id = id;
+        this.customer = customer;
+        this.roomType = roomType;
+        this.checkinDate = checkinDate;
+        this.checkoutDate = checkoutDate;
+        this.price = price;
+        this.voucher = voucher;
+        this.finalPrice = finalPrice;
+        this.paymentStatus = paymentStatus;
+        this.hasCheckedin = hasCheckedin;
+        this.hasCheckedout = hasCheckedout;
+    }
+
+    // Getter & Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(int customer) {
+        this.customer = customer;
+    }
+
+    public int getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(int roomType) {
+        this.roomType = roomType;
+    }
+
+    public LocalDateTime getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(LocalDateTime checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public LocalDateTime getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(LocalDateTime checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Integer getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(Integer voucher) {
+        this.voucher = voucher;
+    }
+
+    public int getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(int finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public boolean isHasCheckedin() {
+        return hasCheckedin;
+    }
+
+    public void setHasCheckedin(boolean hasCheckedin) {
+        this.hasCheckedin = hasCheckedin;
+    }
+
+    public boolean isHasCheckedout() {
+        return hasCheckedout;
+    }
+
+    public void setHasCheckedout(boolean hasCheckedout) {
+        this.hasCheckedout = hasCheckedout;
+    }
+
+    // Optional: toString()
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", roomType=" + roomType +
+                ", checkinDate=" + checkinDate +
+                ", checkoutDate=" + checkoutDate +
+                ", price=" + price +
+                ", voucher=" + voucher +
+                ", finalPrice=" + finalPrice +
+                ", paymentStatus='" + paymentStatus + '\'' +
+                ", hasCheckedin=" + hasCheckedin +
+                ", hasCheckedout=" + hasCheckedout +
+                '}';
     }
 }
