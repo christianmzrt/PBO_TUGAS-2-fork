b48dbd350393cdacc0c0842e22fcd906c74048ca
bintang <kadekbintang443@gmail.com>
2025-07-01 07:36:11 +0800

    refaktor nama package menjadi ServerLauncher

---
+++ src/ServerLauncher.java
@@ -0,0 +1,13 @@
+import Httpserver.Server;
+
+public class ServerLauncher {
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        System.out.printf("Listening on port: %s...\n", port);
+        new Server(port);
+    }
+}
